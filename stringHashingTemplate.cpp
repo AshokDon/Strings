0fc1923f112a1fd9836865bea38276d54504a2cb
Ashok <68994450+AshokDon@users.noreply.github.com>
2025-06-03 10:10:08 +0530

    Update stringHashingTemplate.cpp

--- stringHashingTemplate.cpp
+++ stringHashingTemplate.cpp
@@ -34,64 +34,62 @@ ll mminvprime(ll a, ll m) {
     return binpow(a, m - 2, m);
 }
 
-struct Hashing {
+struct Hashing{
     string s;
     int n;
     int primes;
-    vector<ll> hashPrimes = {1000000009, 1000000007};  // 2 large primes
+    vector<ll> hashPrimes = {1000000009, 100000007};
     const ll base = 31;
     vector<vector<ll>> hashValues;
     vector<vector<ll>> powersOfBase;
     vector<vector<ll>> inversePowersOfBase;
-
-    Hashing(string a) {
-        s = a;
-        n = s.length();
-        primes = hashPrimes.size();
+    Hashing(string a){
+        primes =hashPrimes.size();
         hashValues.resize(primes);
         powersOfBase.resize(primes);
         inversePowersOfBase.resize(primes);
-
-        for (int i = 0; i < primes; i++) {
+        s = a;
+        n = s.length(); 
+        for(int i = 0; i < hashPrimes.size(); i++) {
             powersOfBase[i].resize(n + 1);
             inversePowersOfBase[i].resize(n + 1);
             powersOfBase[i][0] = 1;
-            for (int j = 1; j <= n; j++) {
-                powersOfBase[i][j] = mod_mul(base, powersOfBase[i][j - 1], hashPrimes[i]);
+            for(int j = 1; j <= n; j++){
+                powersOfBase[i][j] = (base * powersOfBase[i][j - 1]) % hashPrimes[i];
             }
             inversePowersOfBase[i][n] = mminvprime(powersOfBase[i][n], hashPrimes[i]);
-            for (int j = n - 1; j >= 0; j--) {
+            for(int j = n - 1; j >= 0; j--){
                 inversePowersOfBase[i][j] = mod_mul(inversePowersOfBase[i][j + 1], base, hashPrimes[i]);
-            }
+            } 
         }
-
-        for (int i = 0; i < primes; i++) {
+        for(int i = 0; i < hashPrimes.size(); i++) {
             hashValues[i].resize(n);
-            for (int j = 0; j < n; j++) {
-                hashValues[i][j] = mod_mul(s[j] - 'a' + 1, powersOfBase[i][j], hashPrimes[i]);
-                if (j > 0)
-                    hashValues[i][j] = mod_add(hashValues[i][j], hashValues[i][j - 1], hashPrimes[i]);
+            for(int j = 0; j < n; j++){
+                hashValues[i][j] = ((s[j] - 'a' + 1LL) * powersOfBase[i][j]) % hashPrimes[i];
+                hashValues[i][j] = (hashValues[i][j] + (j > 0 ? hashValues[i][j - 1] : 0LL)) % hashPrimes[i];
             }
         }
     }
-
-    // Get hash of substring s[l...r]
-    vector<ll> substringHash(int l, int r) {
+    vector<ll> substringHash(int l, int r){
         vector<ll> hash(primes);
-        for (int i = 0; i < primes; i++) {
-            ll val = hashValues[i][r];
-            if (l > 0)
-                val = mod_sub(val, hashValues[i][l - 1], hashPrimes[i]);
-            hash[i] = mod_mul(val, inversePowersOfBase[i][l], hashPrimes[i]);
+        for(int i = 0; i < primes; i++){
+            ll val1 = hashValues[i][r];
+            ll val2 = l > 0 ? hashValues[i][l - 1] : 0LL;
+            hash[i] = mod_mul(mod_sub(val1, val2, hashPrimes[i]), inversePowersOfBase[i][l], hashPrimes[i]);
         }
         return hash;
     }
 };
 int main() {
-    string str = "abcabc";
+    string str = "abc";
     Hashing H(str);
-    auto h1 = H.substringHash(0, 2);  // hash of "abc"
-    auto h2 = H.substringHash(3, 5);  // hash of second "abc"
-    if (h1 == h2) cout << "Equal\n";  // should print "Equal"
-    else cout << "Not Equal\n";
+    int n = str.length();
+    set<vector<ll>>unique_str;
+    for(int i = 0 ; i < n ; i++){
+        for(int j = i ; j < n ; j++){
+            vector<ll>hash = H.substringHash(i,j);
+            unique_str.insert(hash);
+        }
+    }
+    cout << unique_str.size();
 }
